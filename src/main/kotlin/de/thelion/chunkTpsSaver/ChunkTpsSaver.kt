6fce9537f4eaf81c0a30a9036c3cf640baae4778
thelion2009 <thorbenhess1@gmx.de>
2025-10-29 16:33:53 +0100

    new plugin as a beta

---
+++ src/main/kotlin/de/thelion/chunkTpsSaver/ChunkTpsSaver.kt
@@ -0,0 +1,14 @@
+package de.thelion.chunkTpsSaver
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChunkTpsSaver : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
